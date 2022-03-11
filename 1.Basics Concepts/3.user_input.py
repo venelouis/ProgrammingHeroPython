3f814c3d97f5914002ef3de80c980c1e82c6fab7
Venelouis <37526748+venelouis@users.noreply.github.com>
2022-03-11 16:33:51 -0300

    Add files via upload

---
+++ 1.Basics Concepts/3.user_input.py
@@ -0,0 +1,14 @@
+# input in a variable
+name = input("who is your best friend? "
+print("Best Friend: " + name)
+
+# input numbers:
+siblings_text = input("what is the number of siblings you have? ")
+siblings = int(siblings_text)
+print(siblings)
+
+#or
+
+x = '13.25"
+z = float(x) + 100
+print(z)
